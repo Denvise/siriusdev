cafd0de5a16b7a91406fbb78c0b758a10cccd166
Denvise <jardisindustrie@gmail.com>
2017-09-06 17:42:31 +0200

    style

--- src/SD/CoreBundle/Controller/CoreController.php
+++ src/SD/CoreBundle/Controller/CoreController.php
@@ -8,6 +8,13 @@ class CoreController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('SDCoreBundle:Default:layout.html.twig');
+
+        $datetime1 = new \DateTime('now');
+        $datetime2 = new \DateTime('1991-03-31');
+        $interval = $datetime1->diff($datetime2);
+        $age = $interval->format('%Y%');
+        return $this->render('SDCoreBundle::index.html.twig', [
+            'age' => $age
+        ]);
     }
 }
